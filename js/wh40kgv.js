5885f04cacde12929da2ff84718ec91c8c0a062e
cedricms <cedric@maria-sube.com>
2014-06-19 21:19:20 +0200

    Init camera orientation.

--- js/wh40kgv.js
+++ js/wh40kgv.js
@@ -16,36 +16,45 @@ function loadGalaxyViewer() {
   scene = new THREE.Scene();
 
   // Initialize the the camera and put it in the scene
-  camera = new THREE.PerspectiveCamera(50, viewerWidth / viewerHeight, 1, 10000 );
-  camera.position.set(0, 0, 3000);
-  /*camera.rotation = new THREE.Euler(0, 1.57, -1.57);*/
+  camera = new THREE.PerspectiveCamera(50, viewerWidth / viewerHeight, 1, 4000 );
+  camera.position.set(0, 2500, 0);
+  camera.rotation.x = -90 * Math.PI / 180
   scene.add(camera);
   
-  var gridSize = 1000;
-  var step = 1;
-  var gridHelper = new THREE.GridHelper(gridSize, step);
-
-  gridHelper.position = new THREE.Vector3( 0, 0, 0 );
-  gridHelper.rotation = new THREE.Euler( 0, 0, 0 );
-  scene.add( gridHelper );
-		
-  // Set up scene elements
-  var geometry = new THREE.SphereGeometry(10, 8, 8);
-  var material = new THREE.MeshLambertMaterial( { color: 0xffff00} );
-  mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
+  var galaxy = initGalaxyGeometry();
+  scene.add(galaxy);
 
   // Set up lighting
-  var lumiere = new THREE.DirectionalLight( 0xffffff, 1.0 );
-  lumiere.position.set( 0, 0, 400 );
-  scene.add( lumiere );
+  var light = new THREE.DirectionalLight(0xffffff, 1.0);
+  light.position.set(0, 0, 400);
+  scene.add(light);
 
   // Render the scene
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 function render() {
   animation.update(.01);
   renderer.render(scene, camera);
   requestAnimationFrame(render);
+}
+
+function initGalaxyGeometry() {
+  var galaxy = new THREE.Object3D();
+  
+  var gridSize = 1000;
+  var step = 100;
+  var gridHelper = new THREE.GridHelper(gridSize, step);
+
+  gridHelper.position = new THREE.Vector3( 0, 0, 0 );
+  gridHelper.rotation = new THREE.Euler( 0, 0, 0 );
+  galaxy.add( gridHelper );
+		
+  // Set up scene elements
+  var geometry = new THREE.SphereGeometry(10, 8, 8);
+  var material = new THREE.MeshLambertMaterial( { color: 0xffff00} );
+  mesh = new THREE.Mesh( geometry, material );
+  galaxy.add( mesh );
+  
+  return galaxy;
 }
